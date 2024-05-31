d2a69b77a9adcc524c9b95769f1a2bd490802cb0
briancastro-bc <brian.castro.bedoya@gmail.com>
2024-05-30 19:46:15 -0500

    feat(deploy) config to set up deploy

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+/**
+ * 
+ * Guarda valores en el local storage;
+ * 
+ */
+const set = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+/**
+ * 
+ * Obtener valores almacenados en el local storage;
+ * 
+ */
+const get = (key) => {
+  if (!key) {
+    throw new Error("No hay una llave para consultar");
+  }
+
+  const value = localStorage.getItem(key);
+  return value;
+};
+
+const themeBtn = document.getElementById('theme');
 const counterElement = document.getElementById('counter');
 const incrementBtn = document.getElementById('increment');
 const decrementBtn = document.getElementById('decrement');
-
-/**
-*
-* asdljasldkjasdkljaskdljaskd
-*
-*/
-
-console.log('Hello world');
 
 let counter = 0;
 
@@ -18,13 +34,53 @@ const renderCounter = () => {
 
 const increment = () => {
   counter++;
+  set('counter', counter);
   renderCounter();
 };
 
 const decrement = () => {
   counter--;
+  set('counter', counter);
   renderCounter();
-}
+};
+
+const changeTheme = () => {
+  let theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    set('theme', 'light');
+  } else {
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    set('theme', 'dark');
+  }
+};
 
 incrementBtn.onclick = increment;
 decrementBtn.onclick = decrement;
+themeBtn.onclick = changeTheme;
+
+const load = () => {
+  const storedCounter = +localStorage.getItem('counter') ?? 0;
+
+  counter = storedCounter;
+
+  const userThemePreference = window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .matches
+
+  if (userThemePreference) {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    set('theme', 'dark');
+  } else {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    set('theme', 'light');
+  }
+
+  renderCounter();
+};
+
+window.onload = load;
